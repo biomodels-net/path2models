45e6577cfbd4fecbef2557a9d2e4ad3b2a0a67d7
Michael Schubert <mschu.dev@gmail.com>
2011-12-05 13:42:34 +0000

    read layout extension

--- SbgnLayout/src/SbgnLayout/Main.java
+++ SbgnLayout/src/SbgnLayout/Main.java
@@ -16,31 +16,16 @@ public class Main {
             SbgnIO sbgnIO = SbgnIO.fromSbmlQual("hsa04210.sbml.xml");
             
             KeggLayout kl = new KeggLayout(sbgnIO.getNetwork());
-            kl.applyCoordFile("hsa04210.coords.txt");
+        //    kl.applyCoordFile("hsa04210.coords.txt");
         //    kl.applyKeggFile("hsa04210.xml");
             sbgnIO.writeToFile("kegg.sbgn");
             
-            ChisioLayout cl1 = new ChisioLayout(sbgnIO.getNetwork(), LayoutAlgorithm.CoSE);
-            sbgnIO.writeToFile("chi_cose.sbgn");
-            
-            ChisioLayout cl2 = new ChisioLayout(sbgnIO.getNetwork(), LayoutAlgorithm.CiSE);
-            sbgnIO.writeToFile("chi_cise.sbgn");
-
-            ChisioLayout cl3 = new ChisioLayout(sbgnIO.getNetwork(), LayoutAlgorithm.SixCircular);
-            sbgnIO.writeToFile("chi_sixcircular.sbgn");
-            
-            ChisioLayout cl4 = new ChisioLayout(sbgnIO.getNetwork(), LayoutAlgorithm.Cluster);
-            sbgnIO.writeToFile("chi_cluster.sbgn");
-
             ChisioLayout cl5 = new ChisioLayout(sbgnIO.getNetwork(), LayoutAlgorithm.Sugiyama);
             sbgnIO.writeToFile("chi_sugiyama.sbgn");
-            
-            ChisioLayout cl6 = new ChisioLayout(sbgnIO.getNetwork(), LayoutAlgorithm.Spring);
-            sbgnIO.writeToFile("chi_spring.sbgn");
 
-            JGraphLayout jgf = new JGraphLayout(sbgnIO.getNetwork());
-            jgf.applyHierarchical();
-            sbgnIO.writeToFile("jg_hierarchical.sbgn");
+        //    JGraphLayout jgf = new JGraphLayout(sbgnIO.getNetwork());
+        //    jgf.applyHierarchical();
+        //    sbgnIO.writeToFile("jg_hierarchical.sbgn");
         }
         catch (Exception e) {
             e.printStackTrace();
--- SbgnLayout/src/SbgnLayout/SbmlQualIO.java
+++ SbgnLayout/src/SbgnLayout/SbmlQualIO.java
@@ -8,6 +8,7 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import javax.xml.stream.XMLStreamException;
@@ -22,9 +23,15 @@ import org.sbgn.bindings.Label;
 import org.sbgn.bindings.Map;
 import org.sbgn.bindings.Point;
 import org.sbgn.bindings.Sbgn;
+import org.sbml.jsbml.ListOf;
 import org.sbml.jsbml.Model;
 import org.sbml.jsbml.SBMLDocument;
 import org.sbml.jsbml.SBMLReader;
+import org.sbml.jsbml.ext.layout.BoundingBox;
+import org.sbml.jsbml.ext.layout.Dimensions;
+import org.sbml.jsbml.ext.layout.ExtendedLayoutModel;
+import org.sbml.jsbml.ext.layout.Layout;
+import org.sbml.jsbml.ext.layout.SpeciesGlyph;
 import org.sbml.jsbml.ext.qual.Input;
 import org.sbml.jsbml.ext.qual.Output;
 import org.sbml.jsbml.ext.qual.QualConstant;
@@ -37,24 +44,31 @@ import org.sbml.jsbml.ext.qual.Transition;
  * @author mschu
  */
 public class SbmlQualIO {
-    
-    private QualitativeModel model;
+    private Model baseModel;
+    private QualitativeModel qualModel;
     
     public SbmlQualIO(String filename) throws XMLStreamException, IOException {
         SBMLReader sbmlRead = new SBMLReader();
         SBMLDocument sbmlDoc = sbmlRead.readSBMLFromFile(filename);
-        Model baseModel = sbmlDoc.getModel();
-        model = (QualitativeModel)baseModel.getExtension(QualConstant.namespaceURI);
+        baseModel = sbmlDoc.getModel();
+        qualModel = (QualitativeModel)baseModel.getExtension(QualConstant.namespaceURI);
     }
     
-    public Sbgn createSBGN() {
+    public Sbgn createSBGN() {     
         Sbgn sbgn = new Sbgn();		
         Map map = new Map();
         sbgn.setMap(map);
         List<Glyph> glyphs = map.getGlyph();
         
+        ListOf<QualitativeSpecies> qualSpeciesList = qualModel.getListOfQualitativeSpecies();
+        ArrayList<String> qsNameList = new ArrayList<String>();
+        for (QualitativeSpecies qs : qualSpeciesList) {
+            qsNameList.add(qs.getName());
+        }
+        HashMap<String, BoundingBox> layoutLookup = getLayoutMapOrNull(qsNameList);
+        
         HashMap<String, Glyph> glyphLookup = new HashMap<String, Glyph>();
-        for (QualitativeSpecies qs : model.getListOfQualitativeSpecies()) {            
+        for (QualitativeSpecies qs : qualSpeciesList) {            
             Glyph glyph = new Glyph();
             glyph.setId(qs.getId());
             glyph.setClazz(sbo2GlyphClazz(qs.getSBOTerm()));
@@ -64,18 +78,28 @@ public class SbmlQualIO {
             glyph.setLabel(label);
             
             Bbox bbox = new Bbox();
-            bbox.setX(0);
-            bbox.setY(0);
-            Point pt = label2Size(qs.getName());
-            bbox.setW(pt.getX());
-            bbox.setH(pt.getY());
+            if (layoutLookup != null) {
+                BoundingBox box = layoutLookup.get(qs.getName());
+                org.sbml.jsbml.ext.layout.Point pt = box.getPosition();
+                bbox.setX((float)pt.getX());
+                bbox.setY((float)pt.getY());
+                Dimensions dim = box.getDimensions();
+                bbox.setH((float)dim.getHeight());
+                bbox.setW((float)dim.getWidth());
+            } else {
+                bbox.setX(0);
+                bbox.setY(0);
+                Point pt = label2Size(qs.getName());
+                bbox.setW(pt.getX());
+                bbox.setH(pt.getY());
+            }
             glyph.setBbox(bbox);
             
             glyphLookup.put(glyph.getId(), glyph);
             glyphs.add(glyph);
         }
         
-        for (Transition trans : model.getListOfTransitions()) {
+        for (Transition trans : qualModel.getListOfTransitions()) {
             for (Output output : trans.getListOfOutputs()) {
                 for (Input input : trans.getListOfInputs()) {
                     Arc arc = new Arc();
@@ -101,9 +125,31 @@ public class SbmlQualIO {
         return sbgn;
     }
     
+    private HashMap<String, BoundingBox> getLayoutMapOrNull(ArrayList<String> qsNameList) {
+        final String LAYOUT_NS = "http://www.sbml.org/sbml/level3/version1/layout/version1";
+        ExtendedLayoutModel layout = (ExtendedLayoutModel)baseModel.getExtension(LAYOUT_NS);
+        if (!layout.isSetListOfLayouts()) {
+            return null;
+        }
+        
+        Layout l = layout.getLayout(0);
+        HashMap<String, BoundingBox> layoutMap = new HashMap<String, BoundingBox>();
+        for (SpeciesGlyph sg: l.getListOfSpeciesGlyphs()) {
+            if (sg.isSetBoundingBox()) {
+                layoutMap.put(sg.getSpecies(), sg.getBoundingBox());
+            }
+        }
+        
+        for (String qs : qsNameList) {
+            if (!layoutMap.keySet().contains(qs)) {
+                return null;
+            }
+        }
+        return layoutMap;
+    }
+    
     private Point label2Size(String label) {
-        Font font = new Font("Arial", Font.PLAIN, 10);  
-   //     FontMetrics metrics = new FontMetrics(font) {};
+        Font font = new Font("Arial", Font.PLAIN, 10);
         BufferedImage bi = new BufferedImage(5, 5, BufferedImage.TYPE_INT_RGB);
         FontMetrics metrics = bi.getGraphics().getFontMetrics(font);
 
