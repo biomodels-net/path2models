b2ede2461073cd6c0fca946464f15b8ea38e3ad4
Michael Schubert <mschu.dev@gmail.com>
2012-04-25 14:43:23 +0000

    refactoring

--- adaptagrams/main.cpp
+++ adaptagrams/main.cpp
@@ -1,19 +1,10 @@
 //#define QT_NO_DEBUG_OUTPUT
 
-#include <Qt/qdebug.h>
 #include "SBMLQualParser.h"
 #include "AdaptagramsLayout.h"
 #include "SBGNWriter.h"
 
 using namespace SBMLQual;
-
-template<class T> int getIndexById(T iterable, string id) {
-    return find(iterable.begin(), iterable.end(), id.c_str())-iterable.begin();
-}
-
-template<class T> T getObjectById(std::vector<T> iterable, string id) {
-    return find(iterable.begin(), iterable.end(), id.c_str());
-}
 
 int main(int argc, char* argv[]) {
     Parser parser = Parser(argc, argv);
@@ -22,39 +13,12 @@ int main(int argc, char* argv[]) {
 
     AdaptagramsLayout layout = AdaptagramsLayout();
     layout.constructFromSBMLQual(sp, tr);
-
-    foreach(AmbiguousMatch am, parser.getAmbiguousPositions()) {
-        int idx = getIndexById(sp, am.id);
-        Rectangle *r = layout.getRectangleByIndex(idx);
-
-        double oldStress = layout.computeStress();
-        assert(am.x.size()==am.y.size());
-        for(int i=1; i<am.x.size(); i++) {
-            layout.setNodePositionByIndex(idx, am.x[i], am.y[i]);
-            double newStress = layout.computeStress();
-            if (newStress > oldStress) {
-                layout.setNodePositionByIndex(idx, am.x[i-1], am.y[i-1]);
-            }
-            else {
-                oldStress = newStress;
-                qDebug() << QString(am.id.c_str()) << "moved to (" << r->getMinX() \
-                    << "," << r->getMinY() << "); new stress:" << newStress;
-            }
-        }
-    }
+    layout.iterateAmbiguousPositions(sp, parser.getAmbiguousPositions());
 
     vector<Rectangle*> rs = layout.getRectangles();
     vector<Edge> es = layout.getEdges();
 
-    assert(es.size()==tr.size());
-    for(int i=0; i<es.size(); i++) {
-        Edge e = es[i];
-        Rectangle* from = rs[e.first];
-        Rectangle* to = rs[e.second];
-        tr[i].addPoint(from->getMinX(), from->getMinY());
-        tr[i].addPoint(to->getMinX(), to->getMinY());
-    }
-
+    tr = layout.anchorEdges(tr); // TODO: pointers
     SBGNWriter writer = SBGNWriter(rs, es, sp, tr);
     writer.writeFile(argv[2]);
 }
